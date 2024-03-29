8434651880d0427499b47ef6d648c35ad1d90841
Manishkumarchoudhary2003 <cmanishkumar193@gmail.com>
2024-03-29 18:48:06 +0530

    removing comment

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -5,7 +5,6 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import ListItemText from "@material-ui/core/ListItemText";
-// import Checkbox from '@material-ui/core/Checkbox';
 import IconButton from "@material-ui/core/IconButton";
 import EditIcon from "@material-ui/icons/Edit";
 import DeleteIcon from "@material-ui/icons/Delete";
