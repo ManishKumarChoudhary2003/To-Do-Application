5ea271f57cfbc94ce53632263a47984ace3ee44f
Manishkumarchoudhary2003 <cmanishkumar193@gmail.com>
2024-03-29 18:16:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1 style={{ marginBottom: "30px" }}>React To-Do Application</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
